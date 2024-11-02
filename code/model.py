f3959ccb0907ee62bba51ca6d2059d3308c6234e
SpeltIncorrectyl <speltincorrectyl0001@gmail.com>
2024-11-02 15:26:00 +0000

    removed pointless python file

--- code/model.py
+++
@@ -1,7 +0,0 @@
-class Feedback(db.Model):
-    id=db.Column(db.Integer,primary_key=True)
-    date = db.Column(db.Date)
-    time = db.Column(db.String)
-    comment = db.Column(db.String)
-    upvote = db.Column(db.Integer)
-    downvote = db.Column(db.Integer)
